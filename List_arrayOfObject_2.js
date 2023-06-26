64496ed3cdb650f4f4f97e8c0276ae110493aaaa
Fahim Rahman <ifahim.rahman@gmail.com>
2023-06-26 10:23:58 +0600

    Create List_arrayOfObject_2.js

---
+++ List_arrayOfObject_2.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+const books =[{
+    img: "https://images-na.ssl-images-amazon.com/images/I/A1bc9KkHtsL._AC_UL254_SR254,254_.jpg",
+    title: "Iron Flame (The Empyrean, 2)",
+    author:"Rebecca yarros"
+},
+{
+    img: "https://images-na.ssl-images-amazon.com/images/I/A1bc9KkHtsL._AC_UL254_SR254,254_.jpg",
+    title: "Iron Flame (The Empyrean, 2)",
+    author:"Rebecca yarros"
+},
+{
+    img: "https://images-na.ssl-images-amazon.com/images/I/A1bc9KkHtsL._AC_UL254_SR254,254_.jpg",
+    title: "Iron Flame (The Empyrean, 2)",
+    author:"Rebecca yarros"
+}]
+
+
+const BookList =()=>{
+    return(
+        <section>
+            {books.map((book)=>{
+                return(
+                    <Book book={book}/>
+                )
+            })}
+        </section>
+    )
+    
+}
+
+const Book =(props)=>{
+    const {img,title,author} =props.book
+    return(
+    <article>
+        <img src={img} alt=""></img>
+        <h1>{title}</h1>
+        <h4>{author}</h4>
+    </article>
+    )
+   
+}
+
+
+
+const Container = document.querySelector('#root')
+
+const root = createRoot(Container)
+root.render(<BookList/>)
